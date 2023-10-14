0e34c71e9a74607e29c618a14a1baec85107f7e6
AndersonShen <shen.andersonshen@gmail.com>
2023-10-14 13:26:28 +0000

    init

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/2023_web-layouting_week8/',
+  plugins: [vue()],
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
